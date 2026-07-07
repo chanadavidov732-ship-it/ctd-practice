971d47b76b7209b12e2170ab1ae2a0c29a357b80
Ayala <ayala@example.com>
2026-07-07 14:32:48 +0300

    ayala

--- project/Program.cs
+++ project/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("ayala & chana");
+            Console.WriteLine("ayala togezer chana");
         }
     }
 }

ecf7694edb18b6b6d3d7cfcf5c1b0493701c6847
חנה דוידוב <[200~chana88732@gmail.com>
2026-07-07 14:38:20 +0300

    chana

--- project/obj/Debug/net8.0/project.AssemblyInfo.cs
+++ project/obj/Debug/net8.0/project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d594891bb8e0d1566f98dac228b5522c8fa86f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

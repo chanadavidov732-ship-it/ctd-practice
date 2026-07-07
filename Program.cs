3dbc55b1213de6fc77b3f9ae25bebfec41895d9a
חנה דוידוב <[200~chana88732@gmail.com>
2026-07-07 14:20:14 +0300

    add text

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("ayala & chana");
+        }
+    }
+}
